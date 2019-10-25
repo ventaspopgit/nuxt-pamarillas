918b07321cb8e36c3c068004d62cec327950d354
Federico García <federico@Fede-iMac.local>
2019-10-25 14:17:11 -0300

    Actualizar nombre.

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Geelbe en MODO OUTLET',
+    title: 'Páginas Amarillas',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
